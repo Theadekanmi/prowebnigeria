078dbe63d6fd86ce572f06567b4e07844cbf9784
Theadekanmi <theadekanmi@gmail.com>
2025-09-19 15:36:58 -0500

    Fix: Switch to static export and properly ignore node_modules

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    outputFileTracingRoot: undefined
-  },
-  output: 'standalone'
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
